f5f6d4bdde9d6cca54c35e3d3ac72f22eb6b91a6
huaweiwx <huaweiwx@sina.com>
2018-09-26 22:16:49 +0800

    move to adc folder

---
+++ hardware/arm/EFM32/libraries/A_Exaples/examples/5 Extension/adc/adcDiffRead/adcDiffRead.ino
@@ -0,0 +1,39 @@
+/*
+  adcDiffRead.ino  efm32adc differential read demo
+     ADC class func list:
+         ADC adcinstence         : creat a ADC instance,  adc is predefined
+         resolution(res)         :set resolution
+         getResolution()         :get resolution
+         reference(int ref)      :set reference
+         getReference()          :get reference
+         read(pin1, pin2)        ;pin2 default is NOT_A_PIN,SingleInput else deffInput mode
+         readTemp()              :read internal temperature ADC value;
+         temperatureCelsius()    :temperature degree celsius;
+         temperatureFahrenheit() :temperature degree fahrenheit;
+
+    This example code is in the public domain.
+    huaweiwx@sina.com 2018.8
+*/
+
+void setup() {
+   pinMode(LED_BUILTIN, OUTPUT);
+
+  /* setup alternate location default is 0# */
+  Serial.setRouteLoc(1); /*set to 1#. the serial port's Alternate LOCATION(see datasheet)*/
+  Serial.begin(115200);
+
+  adc.reference(INTERNAL2V5); /* setup ADC reference INTERNAL1V25/INTERNAL2V5/INTERNAL3V3*/
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalToggle(LED_BUILTIN);   // turn the LED on (HIGH is the voltage level)
+#if defined(A4 )&& defined(A5)  /* adc channel see datasheet */
+  Serial.print("ADC differential ch4 ch5 read:");
+  Serial.println(adc.read(A4, A5));
+#else
+  Serial.print("ADC differential ch6 ch7 read:");
+  Serial.println(adc.read(A6, A7));
+#endif
+  delay(2000);  // wait for 2 seconds
+}
--- hardware/arm/EFM32/libraries/A_Exaples/examples/5 Extension/adc/temperatureSensor/temperatureSensor.ino
+++ hardware/arm/EFM32/libraries/A_Exaples/examples/5 Extension/adc/temperatureSensor/temperatureSensor.ino
@@ -1,7 +1,8 @@
 /*
-  adcTempRead.ino  efm32adc temperature read demo
+  temperatureSensor.ino   read chip temperature Sensor value
+  use extension temperatureFahrenheit and temperatureCelsius get the temperature and prints it to the Serial Monitor or plotter
   This example code is in the public domain.
-  huaweiwx@sina.com 2018.8
+  huaweiwx@sina.com 2018.9.18
 */
 
 // the setup function runs once when you press reset or power the board
@@ -23,16 +24,16 @@ void loop() {
 #if 1  /*set 0  use plotter */
 /* serial output temperature */
   Serial.print("Read internal temperature :");
-  float temp = adc.fahrenheitTemp();
+  float temp = adc.temperatureFahrenheit();
   Serial.print(temp);
   Serial.print("F (");
-  temp = adc.celsiusTemp();
+  temp = adc.temperatureCelsius();
   Serial.print(temp);
   Serial.println("C)");
   delay(2000);                  // wait for 2 seconds
  #else
  /*plotter temperature curver*/
-   Serial.println(adc.celsiusTemp());
+   Serial.println(adc.temperatureCelsius());
    delay(100);
  #endif
 }
